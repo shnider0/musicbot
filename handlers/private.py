1a76234a4e3c0f2e5e472a5230f3f3ccc1ddd9f8
matrix <64998320+shnider0@users.noreply.github.com>
2021-04-07 03:06:04 +0300

    Delete private.py

--- handlers/private.py
+++
@@ -1,36 +0,0 @@
-from pyrogram import Client
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-
-from config import BOT_NAME as bn
-from helpers.filters import other_filters2
-
-
-@Client.on_message(other_filters2)
-async def start(_, message: Message):
-    await message.reply_text(
-        f"""I am **{update.effective_user.first_name}** !!
-اهلا بك في بوت الموسيقى 
-😉
-
-الاوامر الخاصة بتشغيل البوت في المجموعة :
-⚜️ /song  __ امر بحث النغمة المطلوبة
-⚜️ /play - __لتشغيل الملفات النغمة بواسطة الرد او رابط يوتيوب .__
-⚜️ /pause - __ايقاف صوت الموسيقى مؤقتا.__
-⚜️ /resume - __استئناف الموسقى .__
-⚜️ /skip - __الانتقال الى اللموسقى التالية.__
-⚜️ /stop - __توقف الموسيقى.__
-        يمكنك ايضا مراسلة المطور وطلب نسختك الخاصة من البوت """,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "كروب المطور  💬", url="https://t.me/BOT_MUSIC_IQ"
-                    ),
-                    InlineKeyboardButton(
-                        "قناة المطور  📣", url="https://t.me/BOTS_MUSIC_IQ"
-                    )
-                    
-                ]
-            ]
-        )
-    )
