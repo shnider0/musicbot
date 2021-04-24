3189790d863a6fe56da6c98c344922edfa01238b
matrix <64998320+shnider0@users.noreply.github.com>
2021-04-25 00:36:07 +0300

    Add files via upload

--- handlers/start.py
+++ handlers/start.py
@@ -41,3 +41,24 @@ async def start_(client: Client, message: Message):
         )
     )
 
+@Client.on_message(
+    filters.command("start")
+    & filters.group
+    & ~ filters.edited
+)
+async def start(client: Client, message: Message):
+    await message.reply_text(
+        "💁🏻‍♂️ Do you want to search for a YouTube video?",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✅ Yes", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "No ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
